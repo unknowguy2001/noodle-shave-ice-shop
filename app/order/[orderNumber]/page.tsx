9bed1a1bbe05f5fe311a3ee466bba5e1c1ca17b4
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-04-26 20:26:14 +0700

    feat(client): add order list and order page

---
+++ app/order/[orderNumber]/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import porkIcon from "@/public/pork.png";
+const Order = ({ params }: { params: { orderNumber: number } }) => {
+  return (
+    <div className="h-screen">
+      <img src="https://placehold.co/375x250" alt="Menu Image" />
+      <div className="h-full">
+        <div className="grid grid-cols-1 gap-2 pt-4">
+          <div className="flex justify-between px-8 items-center">
+            <Image src={porkIcon} alt="pork" width="50"></Image>
+            <button className="btn btn-outline btn-success">ใส่หมูสับ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;

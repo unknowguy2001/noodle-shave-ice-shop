3c55dfa8f55b9558856f32690a18c50acbb41be2
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-04-30 18:28:31 +0700

    feat(client): add fetch menu to card

--- components/card.tsx
+++ components/card.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Card = () => {
+  const [data, setData] = useState<
+    { name: string; description: string; price: number; imgae: string }[]
+  >([]);
+
+  const fetchData = async () => {
+    const response = await fetch("/api/menu");
+    const products = await response.json();
+
+    return products;
+  };
+
+  useEffect(() => {
+    (async function () {
+      const products = await fetchData();
+
+      setData(products);
+    })();
+  }, []);
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
-      <figure className="pl-8">
-        <img
-          src="https://placehold.co/320x320"
-          alt="Noodles"
-          className="rounded"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">ก๊วยเตี๋ยว</h2>
-        <p>ก๊วยเตี๋ยวแซ่บๆ ใส่หมูสับ ราคา: 30฿</p>
-        <div className="card-actions justify-start items-center">
-          <button className="btn btn-primary">เพิ่มออเดอร์</button>
-        </div>
-      </div>
+      {data.map((product) => (
+        <>
+          <figure className="pl-8">
+            <img
+              src={product.imgae || "https://placehold.co/320x320"}
+              alt={product.name}
+              className="rounded"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{product.name}</h2>
+            <p>{product.description}</p>
+            <div className="card-actions justify-start items-center">
+              <button className="btn btn-primary">เพิ่มออเดอร์</button>
+            </div>
+          </div>
+        </>
+      ))}
     </div>
   );
 };

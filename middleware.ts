21b6a190b62df801a545f61bd3285751b83379cb
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-08 16:31:24 +0700

    refac(server): remove unnecessary import in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,6 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import * as jose from "jose";
-import {
-  RequestCookie,
-  RequestCookies,
-} from "next/dist/compiled/@edge-runtime/cookies";
 
 export async function middleware(request: NextRequest) {
   const cookieStore = cookies();
@@ -29,7 +25,6 @@ export async function middleware(request: NextRequest) {
 
     //* If the token expires.
   } catch (error) {
-    console.log(refreshToken);
     const urlToFetch = "/api/auth/refresh-token";
 
     const response = await fetch(process.env.URL + urlToFetch, {
@@ -52,5 +47,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/dashboard",
+  matcher: ["/admin/:path*"],
 };

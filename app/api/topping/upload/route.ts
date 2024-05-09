8849c62933c5d0cd9072e796fb658e6f88c2a640
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-09 17:05:10 +0700

    feat(server): add create topping route

---
+++ app/api/topping/upload/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import fs from "fs";
+import { createConnection } from "../../../../lib/db";
+import Topping from "../../models/topping.schema";
+
+const UPLOAD_DIR = path.resolve(process.env.ROOT_PATH ?? "", "public/uploads");
+
+export const POST = async (req: NextRequest) => {
+  try {
+    await createConnection();
+
+    const formData = await req.formData();
+    const body = Object.fromEntries(formData);
+    const file = (body.file as Blob) || null;
+
+    const name = body.toppingName;
+    const icon = (body.file as File).name;
+
+    if (file) {
+      const buffer = Buffer.from(await file.arrayBuffer());
+      if (!fs.existsSync(UPLOAD_DIR)) {
+        fs.mkdirSync(UPLOAD_DIR);
+      }
+
+      fs.writeFileSync(
+        path.resolve(UPLOAD_DIR, (body.file as File).name),
+        buffer
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+        },
+        { status: 500 }
+      );
+    }
+    const newTopping = new Topping({
+      name,
+      icon,
+    });
+
+    const result = newTopping.save();
+    return NextResponse.json(
+      { success: true, name: (body.file as File).name },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: (error as Error).message,
+    });
+  }
+};

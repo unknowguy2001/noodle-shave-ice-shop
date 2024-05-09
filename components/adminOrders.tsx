4c03dea5c0fbe1591e9ac1919f9c832b51799f5e
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-09 17:10:03 +0700

    refac(client): update add and ignore button in admin order comp

--- components/adminOrders.tsx
+++ components/adminOrders.tsx
@@ -8,12 +8,44 @@ export const AdminOrders = () => {
         <div>
           <h3>ชื่อ: คุณต๋อย</h3>
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 overflow-hidden items-center">
           <button className="btn btn-xs btn-info sm:btn-sm md:btn-md lg:btn-lg">
             ดูรายละเอียด
           </button>
-          <button className="btn btn-xs btn-success sm:btn-sm md:btn-md lg:btn-lg">
-            เสร็จสิ้นออเดอร์
+          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="green"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-circle-check"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="m9 12 2 2 4-4" />
+            </svg>
+          </button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="red"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-circle-x"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="m15 9-6 6" />
+              <path d="m9 9 6 6" />
+            </svg>
           </button>
         </div>
       </div>

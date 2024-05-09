f110b92d4928eb498fea74cebefe2be46835b6e5
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-09 17:07:10 +0700

    fix(server): fix can't populate toppings in menu

--- app/api/models/menu.schema.ts
+++ app/api/models/menu.schema.ts
@@ -1,9 +1,10 @@
 import mongoose, { Schema, Types, models } from "mongoose";
+import Topping from "./topping.schema"; //! don't forget to import 
 
 const menuSchema = new Schema({
   name: { type: String, required: true },
-  image: {type: String, requried: true},
-  toppings: [{ type: Types.ObjectId, ref: "Topping", required: true }],
+  image: { type: String, requried: true },
+  toppings: [{ type: Types.ObjectId, ref: Topping }],
   price: { type: Number, required: true },
   description: { type: String, required: true },
 });

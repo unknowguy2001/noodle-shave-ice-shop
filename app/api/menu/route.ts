768d15f5c9c08188a1abc2f9252f6f00e84e7d1a
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-09 17:03:51 +0700

    feat(server): add completed create menu route

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,7 +1,8 @@
-import { createConnection } from "@/app/api/lib/db";
+import { createConnection } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import Menu from "@/app/api/models/menu.schema";
-import { menuSchema } from "@/app/api/validataion/menu.schema";
+import fs from "fs";
+import path from "path";
 
 export async function GET() {
   await createConnection();
@@ -17,15 +18,61 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   await createConnection();
+  const UPLOAD_DIR = path.resolve(
+    process.env.ROOT_PATH ?? "",
+    "public/uploads"
+  );
 
   try {
-    const { MenuValue } = await req.json();
+    const formData = await req.formData();
+    const body = Object.fromEntries(formData);
 
-    const data = new Menu(menuSchema.parse(MenuValue));
+    const file = (body.file as Blob) || null;
+    const image = (body.file as File).name;
+    const name = body.name;
+    const description = body.description;
+    const price = body.price;
+    const toppings = JSON.parse(body.toppings as string);
+    console.log(file);
+    if (file) {
+      const buffer = Buffer.from(await file.arrayBuffer());
+      if (!fs.existsSync(UPLOAD_DIR)) {
+        fs.mkdirSync(UPLOAD_DIR);
+      }
 
-    const result = await data.save();
+      fs.writeFileSync(
+        path.resolve(UPLOAD_DIR, (body.file as File).name),
+        buffer
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+        },
+        { status: 500 }
+      );
+    }
+    const newMenu = new Menu({
+      name,
+      image,
+      toppings,
+      price,
+      description,
+    });
+    const result = await newMenu.save();
+    return NextResponse.json({ result, success: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      error: (error as Error).message,
+    });
+  }
+}
 
-    return NextResponse.json({ result }, { status: 200 });
+export async function DELETE(req: NextRequest) {
+  await createConnection();
+
+  try {
   } catch (error) {
     return NextResponse.json({ error: (error as Error).message });
   }

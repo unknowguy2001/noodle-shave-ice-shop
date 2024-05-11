525294c817fb4f3969f6b2e3eb28be98390265d2
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-11 15:15:16 +0700

    chore: update menu interface value

--- interfaces/menu.interface.ts
+++ interfaces/menu.interface.ts
@@ -1,7 +1,12 @@
 interface MenuValue {
+  _id: string;
   name: string;
   image: string;
-  toppings: Array<string>;
+  toppings: Array<{
+    id: string;
+    name: string;
+    icon: string;
+  }>;
   price: Number;
   description: string;
 }

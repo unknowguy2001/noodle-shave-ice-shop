735ef883ae52aca9628a103adab5540ce67e7030
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-15 16:06:03 +0700

    feat(client): add overflow for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Card from "@/components/card";
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col h-[600px] overflow-auto">
       <Card />
     </main>
   );

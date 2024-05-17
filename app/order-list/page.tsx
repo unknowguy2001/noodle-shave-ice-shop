fddf8f1bda304e207dd3de0d268d807cceaf9142
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-17 15:26:42 +0700

    feat(client): add realtime create order

--- app/order-list/page.tsx
+++ app/order-list/page.tsx
@@ -1,10 +1,51 @@
 "use client";
 import Link from "next/link";
 import { useStore } from "@nanostores/react";
-import { $orderDetail, removeMenu } from "@/store/orderId";
+import { $orderDetail, removeMenu } from "@/store/orderDetail.store";
+import React, { useEffect, useState } from "react";
+import { $totalPrice } from "@/store/totalPrice.store";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { socket } from "@/socket";
 
 const OrderList = () => {
   const orders = useStore($orderDetail);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const customerName = (e.target as HTMLFormElement).customerNameInput.value;
+
+    const menus = orders.menus.map((item) => ({
+      menu: item._id,
+      toppings: item.selectedToppings.map((topping) => ({
+        _id: topping._id,
+      })),
+    }));
+
+    const response = await fetch("/api/order-list", {
+      method: "POST",
+      body: JSON.stringify({
+        OrderValue: {
+          orderNumber: orders.orderNumber,
+          customerName,
+          menus,
+          totalPrice: $totalPrice.get(),
+        },
+      }),
+    });
+    const result = await response.json();
+
+    if (result.success) {
+      socket.emit("order", "created order");
+      toast.success("คุณสั่งอาหารเรียบร้อยแล้ว กรุณารอแม่ค้าทำอาหารสักครู่...");
+      router.push("/order-list/ordered");
+    } else {
+      toast.error("เกิดข้อผิดพลาดในการสั่งอาหาร กรุณาลองใหม่อีกครั้งในภายหลัง");
+    }
+  };
+  useEffect(() => {}, []);
 
   return (
     <>
@@ -30,7 +71,7 @@ const OrderList = () => {
         <div className="pt-5 pl-5">
           <h2 className="font-semibold">รายการอาหาร</h2>
           <p className="mb-4">เลขที่รายการอาหาร: {orders.orderNumber}</p>
-          <ul className="flex gap-3 flex-col overflow-auto h-[450px]">
+          <ul className="flex gap-3 flex-col overflow-auto h-[400px]">
             {orders.menus.map((item, index) => (
               <li
                 key={item._id + index}
@@ -38,21 +79,26 @@ const OrderList = () => {
               >
                 <div>
                   <p>{item.name}</p>
-                  {item.selectedToppings.map((e) => (
-                    <span key={e._id + index} className="badge badge-secondary">
-                      {e.name}
-                    </span>
-                  ))}
+                  <div className="flex gap-2">
+                    {item.selectedToppings.map((e) => (
+                      <span
+                        key={e._id + index}
+                        className="badge badge-secondary"
+                      >
+                        {e.name}
+                      </span>
+                    ))}
+                  </div>
                 </div>
                 <div className="flex gap-2">
                   <Link
                     href={`/menu-details/${index}`}
-                    className="btn btn-warning"
+                    className="btn btn-warning btn-xs sm:btn-sm md:btn-md lg:btn-lg"
                   >
                     แก้ไข
                   </Link>
                   <button
-                    className="btn btn-error"
+                    className="btn btn-error btn-xs sm:btn-sm md:btn-md lg:btn-lg"
                     onClick={() => removeMenu(index)}
                   >
                     นำออก
@@ -62,6 +108,30 @@ const OrderList = () => {
             ))}
           </ul>
         </div>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <input
+            className="input input-bordered input-primary w-full max-w-xs "
+            type="text"
+            placeholder="กรุณากรอกชื่อของคุณ"
+            name="customerNameInput"
+          />
+          {orders.menus.length <= 0 ? (
+            <button
+              className="btn btn-disabled"
+              role="button"
+              aria-disabled="true"
+            >
+              ยืนยันออเดอร์
+            </button>
+          ) : (
+            <button
+              className="btn btn-success mt-2 btn-sm sm:btn-sm md:btn-md lg:btn-lg"
+              type="submit"
+            >
+              ยืนยันออเดอร์ {$totalPrice.get()}฿
+            </button>
+          )}
+        </form>
       </div>
     </>
   );
---
+++ server.js
@@ -0,0 +1,34 @@
+import { createServer } from "http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    // ...
+    socket.on("order", async (value) => {
+      const response = await fetch("http://localhost:3000/api/order-list");
+      const data = await response.json();
+      socket.broadcast.emit("data", data);
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});

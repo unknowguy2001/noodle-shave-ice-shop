7278c31d34dde8f94560a1e6cba12fde48d9f201
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-17 15:18:21 +0700

    chore: add status for order model

--- app/api/models/order.schema.ts
+++ app/api/models/order.schema.ts
@@ -1,14 +1,17 @@
 import mongoose, { Schema, Types, models } from "mongoose";
+import Menu from "./menu.schema";
+import Topping from "./topping.schema";
 
 const orderSchema = new Schema({
   orderNumber: { type: String, required: true },
   customerName: { type: String, required: true },
   menus: [
     {
-      menu: { type: Types.ObjectId, ref: "Menu" },
-      toppings: [{ type: Types.ObjectId, ref: "Topping" }],
+      menu: { type: Types.ObjectId, ref: Menu },
+      toppings: [{ type: Types.ObjectId, ref: Topping }],
     },
   ],
+  status: { type: Boolean, default: false },
   totalPrice: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });

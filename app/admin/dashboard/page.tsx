ecc14cd2bceab3e3ce4c076ea747c80532957c44
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-05-18 12:21:25 +0700

    chore: remove admin graph from admin dashboard page

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { AdminGraph } from "@/app/admin/dashboard/_components/adminGraph";
 import { AdminOrders } from "@/app/admin/dashboard/_components/adminOrders";
 
 const Dashboard = () => {
   return (
     <div className="h-full">
-      <AdminGraph></AdminGraph>
       <AdminOrders></AdminOrders>
     </div>
   );
